5ae8658e8a5a832828a4b370beb4c49dc51c42d4
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-06-16 14:06:46 +0200

    linter/prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended'
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		'comma-dangle': 0,
+		indent: ['error', 'tab'],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		semi: ['error', 'always'],
+		'space-before-function-paren': 0,
+		'vue/html-indent': ['error', 'tab'],
+		'vue/singleline-html-element-content-newline': 0
+	}
+};
