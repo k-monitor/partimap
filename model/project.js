0f83178c26a85c208865165937c97cc66bb387e7
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-08-31 13:50:13 +0200

    added project, sheet model classes/fields

--- model/project.js
+++ model/project.js
@@ -3,14 +3,18 @@ class Project {
 	 * @param {Object} data
 	 * @param {Number} data.id
 	 * @param {Number} data.userId
+	 * @param {String} data.slug
 	 * @param {String} data.title
 	 * @param {String} data.description
+	 * @param {String} data.password
 	 */
 	constructor(data) {
 		this.id = data.id;
 		this.userId = data.userId;
+		this.slug = data.slug;
 		this.title = data.title;
 		this.description = data.description;
+		this.password = data.password;
 	}
 };
 
---
+++ model/sheet.js
@@ -0,0 +1,25 @@
+class Sheet {
+	/**
+	 * @param {Object} data
+	 * @param {Number} data.id
+	 * @param {Number} data.projectId
+	 * @param {Number} data.ord
+	 * @param {String} data.title
+	 * @param {String} data.description
+	 * @param {String} data.image
+	 * @param {String} data.survey
+	 * @param {String} data.features
+	 */
+	constructor(data) {
+		this.id = data.id;
+		this.projectId = data.projectId;
+		this.order = data.ord;
+		this.title = data.title;
+		this.description = data.description;
+		this.image = data.image;
+		this.survey = data.survey;
+		this.features = data.features;
+	}
+};
+
+module.exports = Sheet;
