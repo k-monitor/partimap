16a841e8af1ce6bf3b4e5ff5a54aee2773cc3166
Domonkos Csuzdi <csuzdi02@gmail.com>
2021-09-03 19:36:16 +0200

    very basic sheet ordering, only local

---
+++ components/ProjectSheetManager.vue
@@ -0,0 +1,122 @@
+<template>
+	<b-container>
+		<b-card class="shadow-sm ">
+			<template #header>
+				<form @submit.prevent="$emit('addSheet',newSheetTitle); newSheetTitle=''">
+					<div class="input-group">
+						<input
+							v-model="newSheetTitle"
+							class="form-control"
+							placeholder="Új munkalap elnevezése"
+							required
+							type="text"
+						>
+						<div class="input-group-append">
+							<button
+								class="btn btn-outline-success"
+								type="submit"
+							>
+								Hozzáadás
+							</button>
+						</div>
+					</div>
+				</form>
+			</template>
+			<div class="list-group">
+				<NuxtLink
+					v-for="sheet in sheets"
+					:key="sheet.ord"
+					:to="'/admin/project/' + projectId + '/sheet/' + sheet.ord"
+					class="align-items-center list-group-item list-group-item-action"
+				>
+					<strong>{{ sheet.title }}</strong>
+					<span
+						class="material-icons delete m-0 float-right text-danger"
+						@click.prevent="showConfirmModal(sheet)"
+					>
+						delete
+					</span>
+					<span
+						v-if="sheet.ord !== (sheets.length-1)"
+						class="material-icons m-0 float-right "
+						@click.prevent="$emit('moveSheet','down',sheet)"
+					>
+						arrow_downward
+					</span>
+					<span
+						v-if="sheet.ord"
+						class="material-icons m-0 float-right "
+						@click.prevent="$emit('moveSheet','up',sheet)"
+					>
+						arrow_upward
+					</span>
+					<b-badge class="float-left mr-3 my-1 order-number">
+						#{{ sheet.ord + 1 }}
+					</b-badge>
+				</NuxtLink>
+			</div>
+		</b-card>
+	</b-container>
+</template>
+
+<script>
+export default {
+	props: {
+		projectId: {
+			type: Number,
+			default: 0
+		},
+		sheets: {}
+	},
+	data() {
+		return {
+			newSheetTitle: ''
+		};
+	},
+	watch: {
+		sheets(data) {
+			console.log(data);
+		}
+	},
+	methods: {
+		showConfirmModal(sheet) {
+			this.$bvModal.msgBoxConfirm('Biztosan törli a kiválasztott elemet?', {
+				title: 'Megerősítés',
+				size: 'sm',
+				buttonSize: 'sm',
+				okVariant: 'danger',
+				okTitle: 'IGEN',
+				cancelTitle: 'MÉGSEM',
+				footerClass: 'p-2',
+				hideHeaderClose: false,
+				centered: true,
+				autoFocusButton: 'ok'
+			})
+				.then(value => {
+					if (value) {
+						this.$emit('delSheet', sheet);
+					}
+				})
+				.catch(err => {
+					console.log(err);
+				});
+		},
+	}
+};
+</script>
+
+<style scoped>
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  cursor: pointer;
+  opacity: 0.5;
+}
+.material-icons:hover {
+  opacity: 1;
+}
+
+.order-number {
+    letter-spacing: 0.1rem;
+}
+</style>
--- pages/admin/project/_id.vue
+++ pages/admin/project/_id.vue
@@ -1,57 +1,68 @@
 <template>
-	<AdminFrame>
-		<template #header>
-			<NuxtLink to="/admin/projects">Projektek</NuxtLink>
-			<span class="text-muted">&raquo;</span>
-			{{ p.title }}
-		</template>
-
-		<form
-			id="projectForm"
-			@submit.prevent="update"
-		>
-			<div class="form-group">
-				<label for="title">Projekt elnevezése</label>
-				<input
-					id="title"
-					v-model="m.title"
-					class="form-control"
-					required
-					type="text"
-				>
-			</div>
-			<div class="form-group">
-				<label for="descriptoin">Leírás</label>
-				<textarea
-					id="description"
-					v-model="m.description"
-					class="form-control"
-					rows="5"
-				/>
-			</div>
-		</form>
-
-		<template #footer>
-			<div class="d-flex justify-content-end">
-				<button
-					class="btn btn-outline-primary"
-					form="projectForm"
-					type="submit"
-				>
-					Mentés
-				</button>
-			</div>
-		</template>
-	</AdminFrame>
+	<div>
+		<AdminFrame>
+			<template #header>
+				<NuxtLink to="/admin/projects">Projektek</NuxtLink>
+				<span class="text-muted">&raquo;</span>
+				{{ p.title }}
+			</template>
+			<form
+				id="projectForm"
+				@submit.prevent="update"
+			>
+				<div class="form-group">
+					<label for="title">Projekt elnevezése</label>
+					<input
+						id="title"
+						v-model="m.title"
+						class="form-control"
+						required
+						type="text"
+					>
+				</div>
+				<div class="form-group">
+					<label for="descriptoin">Leírás</label>
+					<textarea
+						id="description"
+						v-model="m.description"
+						class="form-control"
+						rows="5"
+					/>
+				</div>
+			</form>
+			<template #footer>
+				<div class="d-flex justify-content-end">
+					<button
+						class="btn btn-outline-primary"
+						form="projectForm"
+						type="submit"
+					>
+						Mentés
+					</button>
+				</div>
+			</template>
+		</AdminFrame>
+		<ProjectSheetManager
+			:project-id="p.id"
+			:sheets="localSheets"
+			@addSheet="addSheet"
+			@delSheet="delSheet"
+			@moveSheet="moveSheet"
+		/>
+		<b-button @click="test()" />
+	</div>
 </template>
 
 <script>
+import { orderBy } from 'lodash';
+
 export default {
 	middleware: ['auth'],
 	async asyncData({ $axios, params, redirect }) {
 		try {
 			const p = await $axios.$get('/api/project/' + params.id);
-			return { p, m: { ...p } };
+			const sheets = await $axios.$get('/api/project/' + params.id + '/sheets');
+			return { p, m: { ...p }, sheets, localSheets: [...sheets] };
 		} catch (err) {
 			redirect('/admin/projects');
 		}
@@ -71,6 +82,56 @@ export default {
 				this.error('Módosítás sikertelen');
 			}
 		},
-	},
+		async addSheet(title) {
+			try {
+				const newSheet = await this.$axios.$put('/api/project/' + this.p.id + '/sheet', {
+					title
+				});
+				this.localSheets.push(newSheet);
+			} catch (error) {
+				this.error('Munkalap hozzáadás sikertelen');
+			}
+		},
+		async delSheet(sheet) {
+			try {
+				await this.$axios.$delete('/api/sheet/' + sheet.id);
+			} catch (error) {
+				this.error('Munkalap törlése sikertelen');
+			}
+			this.localSheets = this.localSheets.filter(function(s) {
+				if (s.id !== sheet.id) {
+					if (s.ord > sheet.ord) {
+						s.ord--;
+					}
+					return true;
+				} else {
+					return false;
+				}
+			});
+		},
+		moveSheet(dir, sheet) {
+			if (dir === 'down') {
+				let otherSheet;
+				this.localSheets.forEach(element => {
+					if (element.ord === (sheet.ord + 1)) {
+						otherSheet = element;
+					}
+				});
+				otherSheet.ord--;
+				sheet.ord++;
+				this.localSheets = orderBy(this.localSheets, 'ord', 'asc');
+			} else if (dir === 'up') {
+				let otherSheet;
+				this.localSheets.forEach(element => {
+					if (element.ord === (sheet.ord - 1)) {
+						otherSheet = element;
+					}
+				});
+				otherSheet.ord++;
+				sheet.ord--;
+				this.localSheets = orderBy(this.localSheets, 'ord', 'asc');
+			}
+		}
+	}
 };
 </script>
