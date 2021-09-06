d62c2b3c443ffd3c6f903fac229c173d214fd874
Domonkos Csuzdi <csuzdi02@gmail.com>
2021-09-06 16:56:42 +0200

    added icons, modified order number style

--- components/ProjectSheetManager.vue
+++ components/ProjectSheetManager.vue
@@ -29,26 +29,34 @@
 					:to="'/admin/project/' + projectId + '/sheet/' + sheet.ord"
 					class="align-items-center list-group-item list-group-item-action"
 				>
+					<span class="float-left mr-3">{{ sheet.ord + 1 }}.</span>
 					<strong>{{ sheet.title }}</strong>
-					<b-badge class="float-left mr-3 my-1 order-number">
+
+					<!-- <b-badge class="float-left mr-3 my-1 order-number">
 						#{{ sheet.ord + 1 }}
-					</b-badge>
+					</b-badge> -->
 					<span
-						class="material-icons delete m-0 float-right text-danger"
+						class="material-icons m-0 mr-1 float-left"
+						@mousedown.prevent="showConfirmModal(sheet)"
+					>
+						{{ getSheetType(sheet) }}
+					</span>
+					<span
+						class="material-icons clickable delete m-0 float-right text-danger"
 						@mousedown.prevent="showConfirmModal(sheet)"
 					>
 						delete
 					</span>
 					<span
 						:class="{'down-arrow-disabled' : sheet.ord == (sheets.length-1)}"
-						class="material-icons m-0 float-right"
+						class="material-icons clickable m-0 float-right"
 						@click.prevent="$emit('moveSheet','down',sheet)"
 					>
 						arrow_downward
 					</span>
 					<span
 						v-if="sheet.ord"
-						class="material-icons m-0 float-right "
+						class="material-icons clickable m-0 float-right "
 						@click.prevent="$emit('moveSheet','up',sheet)"
 					>
 						arrow_upward
@@ -74,6 +82,17 @@ export default {
 		};
 	},
 	methods: {
+		getSheetType(sheet) {
+			if (sheet.image) {
+				return 'image';
+			} else if (sheet.survey) {
+				return 'poll';
+			} else if (sheet.features) {
+				return 'map';
+			} else {
+				return 'border_clear';
+			}
+		},
 		showConfirmModal(sheet) {
 			this.$bvModal.msgBoxConfirm('Biztosan törli a kiválasztott elemet?', {
 				title: 'Megerősítés',
@@ -104,10 +123,13 @@ export default {
 .material-icons {
   font-size: 24px;
   margin-left: 10px;
+}
+
+.material-icons.clickable {
   cursor: pointer;
   opacity: 0.5;
 }
-.material-icons:hover {
+.material-icons.clickable:hover {
   opacity: 1;
 }
 
@@ -115,7 +137,7 @@ export default {
     letter-spacing: 0.1rem;
 }
 
-.down-arrow-disabled {
+.material-icons.down-arrow-disabled {
 	pointer-events: none;
 	opacity: 0;
 }
