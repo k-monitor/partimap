eb9ca2044a58890dfce16fa933c00c6f9a5d1298
Domonkos Csuzdi <csuzdi02@gmail.com>
2021-09-08 23:45:01 +0200

    feature list sidebar collapse, expand

--- components/FeatureListContainer.vue
+++ components/FeatureListContainer.vue
@@ -1,99 +1,91 @@
 
 <template>
-	<div>
-		<b-collapse id="feature-list" v-model="containerVisible">
-			<b-card class="real">
-				<template #header>
-					<div class="add-feature">
-						<b-button-group class="w-100 ">
-							<b-button
-								:class="{ 'btn-success': !editState, 'btn-danger': editState}"
-								@click="changeDrawType()"
-							>
-								<span v-if="!editState" class="material-icons d-flex justify-content-center">
-									add
-								</span>
-								<span v-if="editState" class="material-icons d-flex justify-content-center">
-									close
-								</span>
-							</b-button>
-							<b-dropdown
-								right
-								:text="translateDrawType"
-								class="w-75 add-feature-selector"
-								variant="white"
-								:disabled="editState"
-							>
-								<b-dropdown-item @click="selectedDrawType = 'Point'">Pont</b-dropdown-item>
-								<b-dropdown-item @click="selectedDrawType = 'LineString'">Útvonal</b-dropdown-item>
-								<b-dropdown-item @click="selectedDrawType = 'Polygon'">Terület</b-dropdown-item>
-							</b-dropdown>
-						</b-button-group>
-					</div>
-				</template>
-				<b-card-body class="p-0 m-0">
-					<div class="accordion" role="tablist">
-						<div class="overflow-auto">
-							<b-card-text>
-								<b-list-group>
-									<FeatureListElement
-										v-for="feature in allFeatures"
-										:key="feature.getId()"
-										:feature="feature"
-									/>
-								</b-list-group>
-							</b-card-text>
+	<b-card>
+		<template #header>
+			<div class="add-feature">
+				<b-button-group class="w-100 ">
+					<b-button
+						:class="{ 'btn-success': !editState, 'btn-danger': editState}"
+						@click="changeDrawType()"
+					>
+						<span v-if="!editState" class="material-icons d-flex justify-content-center">
+							add
+						</span>
+						<span v-if="editState" class="material-icons d-flex justify-content-center">
+							close
+						</span>
+					</b-button>
+					<b-dropdown
+						right
+						:text="translateDrawType"
+						class="w-75 add-feature-selector"
+						variant="white"
+						:disabled="editState"
+					>
+						<b-dropdown-item @click="selectedDrawType = 'Point'">Pont</b-dropdown-item>
+						<b-dropdown-item @click="selectedDrawType = 'LineString'">Útvonal</b-dropdown-item>
+						<b-dropdown-item @click="selectedDrawType = 'Polygon'">Terület</b-dropdown-item>
+					</b-dropdown>
+				</b-button-group>
+			</div>
+		</template>
+		<b-card-body class="p-0 m-0">
+			<div class="accordion" role="tablist">
+				<div class="overflow-auto">
+					<b-card-text>
+						<b-list-group>
+							<FeatureListElement
+								v-for="feature in allFeatures"
+								:key="feature.getId()"
+								:feature="feature"
+							/>
+						</b-list-group>
+					</b-card-text>
+				</div>
+			</div>
+			<div class="sidebar-button sidebar-collapse">
+				<a v-b-toggle.map-sidebar href="#">
+					<svg width="14" height="150">
+						<path
+							d=" M 0 150 L 13 135 L 13 15 L 0 0"
+							fill="rgb(247,247,247)"
+							stroke="rgb(223,223,223)"
+						/>
+						<path
+							d="M 0 0 L 0 150"
+						/>
+					</svg>
+				</a>
+				<span class="material-icons collapse-icon">
+					expand_less
+				</span>
+			</div>
+		</b-card-body>
+		<template #footer>
+			<div class="buttons">
+				<b-button variant="warning p-0 mb-1">
+					<div class="content d-flex">
+						<div class="material-icons d-inline  border-info py-1 pl-1">
+							file_upload
+						</div>
+						<div class="text d-inline p-1 pr-2">
+							Import
 						</div>
 					</div>
-				</b-card-body>
-				<template #footer>
-					<div class="buttons mb-3">
-						<b-button variant="warning p-0 mb-1">
-							<div class="content d-flex">
-								<div class="material-icons d-inline  border-info py-1 pl-1">
-									file_upload
-								</div>
-								<div class="text d-inline p-1 pr-2">
-									Import
-								</div>
-							</div>
-						</b-button>
-						<b-button variant="warning p-0 mb-1">
-							<div class="content d-flex">
-								<div class="material-icons d-inline  border-info py-1 pl-1">
-									file_download
-								</div>
-								<div class="text d-inline p-1 pr-2">
-									Export
-								</div>
-							</div>
-						</b-button>
+				</b-button>
+				<b-button variant="warning p-0 mb-1">
+					<div class="content d-flex">
+						<div class="material-icons d-inline  border-info py-1 pl-1">
+							file_download
+						</div>
+						<div class="text d-inline p-1 pr-2">
+							Export
+						</div>
 					</div>
-					<button
-						v-b-toggle.feature-list
-						class="d-block w-100 p-0 collapse-button position-absolute"
-					>
-						<span class="material-icons icon-collapse">
-							expand_less
-						</span>
-					</button>
-				</template>
-			</b-card>
-		</b-collapse>
-		<b-card
-			v-if="!containerVisible"
-			style="height: 20px;"
-		>
-			<button
-				v-b-toggle.feature-list
-				class="d-block w-100 p-0 collapse-button position-absolute"
-			>
-				<span class="material-icons icon-collapse">
-					expand_more
-				</span>
-			</button>
-		</b-card>
-	</div>
+				</b-button>
+			</div>
+		</template>
+	</b-card>
 </template>
 
 <script>
@@ -152,9 +144,8 @@ export default {
 .card-footer {
 	padding: 1rem;
 }
-.card.real {
-    position: relative;
-    height: 85vh;
+.card {
+	height: 100%;
 }
 
 .add-feature-selector {
@@ -163,25 +154,5 @@ export default {
 	border-radius: 0.25rem;
 	border-top-left-radius: 0;
 	border-bottom-left-radius: 0;
-}
-
-.material-icons.icon-collapse{
-	font-size: 20px;
-}
-
-.collapse-button{
-	height: 18px;
-	bottom: 0;
-	left: 0;
-	border:none;
-	border-radius: 0 0 0.25rem 0.25rem;
-	background-color: rgba(0, 0, 0, 0.1)
-}
-.collapse-button:hover{
-		background-color: rgba(0, 0, 0, 0.2)
-}
-.collapse-button:active{
-			background-color: rgba(0, 0, 0, 0.3)
-
 }
 </style>
--- pages/admin/map/_id.vue
+++ pages/admin/map/_id.vue
@@ -14,12 +14,31 @@
 				:features="featuresFromRaw(mapDataServer)"
 			/>
 		</client-only>
-		<b-container class="mt-11 mx-0 ml-auto px-0 px-sm-3 feature-container">
-			<FeatureListContainer
-				:map-title="mapDataLocal.title"
-				@updateTitle="changeMapTitle"
-			/>
-		</b-container>
+		<div>
+			<b-sidebar id="map-sidebar" visible right no-header>
+				<FeatureListContainer
+					:map-title="mapDataLocal.title"
+					@updateTitle="changeMapTitle"
+				/>
+			</b-sidebar>
+			<div class="sidebar-button sidebar-expand">
+				<a v-b-toggle.map-sidebar href="#">
+					<svg width="13" height="150">
+						<path
+							d=" M 13 150 L 0 135 L 0 15 L 13 0"
+							fill="rgb(247,247,247)"
+							stroke="rgb(223,223,223)"
+						/>
+						<path
+							d="M 13 0 L 13 150"
+						/>
+					</svg>
+				</a>
+				<span class="material-icons collapse-icon">
+					expand_more
+				</span>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -39,7 +58,7 @@ export default {
 	},
 	data() {
 		return {
-			mapModified: false
+			mapModified: false,
 		};
 	},
 	head() {
@@ -156,5 +175,46 @@ nem lehet rajta átkattintani. */
 	box-sizing: content-box;
 	min-width: 180px;
 	max-width: 300px;
+}
+
+</style>
+<style>
+
+#map-sidebar {
+	top: 120px;
+	bottom: 50px;
+	height: auto;
+	width: 270px;
+}
+
+@media screen and (max-width: 600px) {
+	#map-sidebar {
+		width: 220px;
+	}
+}
+
+.sidebar-button {
+	position: absolute;
+	top: 50%;
+}
+.sidebar-button.sidebar-expand {
+	right: 0;
+	transform: translate(0, -50%) translateY(35px);
+}
+.sidebar-button.sidebar-collapse {
+	left: 0;
+	transform: translate(0, -50%);
+}
+.sidebar-button:hover path{
+	fill: rgb(223, 223, 223);
+}
+.sidebar-button .collapse-icon {
+	position: absolute;
+	font-size: 15px;
+	font-weight: 700;
+	top: 50%;
+	transform: translate(-15px, -50%) rotate(90deg);
+	pointer-events: none;
+
 }
 </style>
