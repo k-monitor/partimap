343072b1d422f70a79d09adb917916321c535878
Domonkos Csuzdi <csuzdi02@gmail.com>
2021-09-17 15:45:06 +0200

    minor map refactor

--- components/Map.vue
+++ components/Map.vue
@@ -22,8 +22,6 @@ import Collection from 'ol/Collection';
 import Feature from 'ol/Feature';
 import { click } from 'ol/events/condition';
 
-let draw, snap;
-
 export default {
 	props: {
 		initialCenter: {
@@ -42,14 +40,18 @@ export default {
 	},
 	data() {
 		return {
+			// ol/Map object
 			map: null,
 			center: this.initialCenter,
 			zoom: this.initialZoom,
+			// default color for drawn features
 			defaultColor: '#000080',
+			// either be 'Point','LineString', or 'Polygon'
 			drawType: ''
 		};
 	},
 	computed: {
+		// if true, a feature is currently drawn
 		editState() {
 			return this.$store.getters.getEditState;
 		},
@@ -60,21 +62,29 @@ export default {
 		drawType(type) {
 			this.setDrawType(type);
 		},
+		// when edit state is true, feature selection is disabled
 		editState(state) {
 			state ? this.map.removeInteraction(this.select) : this.map.addInteraction(this.select);
 		},
+		/**
+		 * @param selFeature Is null if the map is clicked
+		 * without clicking on a feature.
+		*/
 		getSelectedFeature(selFeature, prevFeature) {
 			// sync the local selectedFeatures collection with the store
 			const selectedFeatures = this.select.getFeatures();
 			if (selFeature && !selectedFeatures.array_.includes(selFeature)) {
 				selectedFeatures.push(selFeature);
 			}
+			// clear the selection of the previously selected feature
 			if (selectedFeatures.array_.includes(prevFeature)) {
 				selectedFeatures.remove(prevFeature);
 			}
+			// deselect the feature if the plain map is clicked
 			if (!selFeature) {
+				// removes the blur which indicates selection
 				this.removeBlur();
-				this.select.getFeatures().pop(); // if a feature is unselected, also clear it from here
+				this.select.getFeatures().pop();
 			} else {
 				this.source.getFeatures().forEach(feature => {
 					if (feature !== selFeature) {
@@ -87,90 +97,20 @@ export default {
 		}
 	},
 	mounted() {
-		const { center, zoom } = this;
-		const raster = new TileLayer({
-			source: new OSM(),
-		});
-
-		this.source = new VectorSource({
-			features: this.loadInitFeatures(this.features)
-		});
-
-		this.vector = new VectorLayer({
-			source: this.source,
-			style: (feature, resolution) => {
-				return this.styleFunction({
-					feature,
-					pointFillColor: this.defaultColor,
-					lineColor: this.defaultColor,
-					polygonColor: this.defaultColor
-				});
-			}
-		});
-		this.select = new Select({
-			style: null,
-			condition: click
-		});
-
-		this.map = new Map({
-			target: this.$refs['map-root'],
-			layers: [raster, this.vector],
-			view: new View({
-				center,
-				constrainResolution: true,
-				zoom,
-			}),
-		});
+		this.initMapComponents();
 		this.fitViewToFeatures();
-
-		this.map.on('moveend', () => {
-			this.center = this.map.getView().getCenter();
-			this.zoom = this.map.getView().getZoom();
-			const { center, zoom } = this;
-			this.$emit('change', {
-				center,
-				zoom,
-			});
-		});
-
-		const selectedFeatures = this.select.getFeatures();
-		selectedFeatures.on('add', f => {
-			if (f !== this.getSelectedFeature) {
-				this.$store.commit('selected/change', f.element);
-			}
-		});
-		selectedFeatures.on('remove', f => {
-			this.$store.commit('selected/remove', f.element);
-		});
-
-		this.source.on('addfeature', f => {
-			f.feature.setId(new Date().getTime());
-			f.feature.set('color', this.defaultColor);
-			this.$store.commit('toggleEditState', false);
-			this.$store.commit('features/add', f.feature);
-
-			selectedFeatures.push(f.feature);
-			this.$nuxt.$emit('mapModified');
-		});
-
-		this.source.on('removefeature', f => {
-			if (f.feature === this.getSelectedFeature) {
-				this.removeBlur();
-			}
-			this.$store.commit('selected/remove', f.feature);
-			this.$store.commit('features/remove', f.feature);
-			this.$nuxt.$emit('mapModified');
-		});
-
-		this.setDrawType(this.drawType);
+		this.addEventListeners();
 	},
 	created() {
+		// handles feature deletion, performed in the feature-sidebar
 		this.$nuxt.$on('clearFeature', feature => {
 			this.vector.getSource().removeFeature(feature);
 		});
+		// handles feature style change, performed in the feature-sidebar
 		this.$nuxt.$on('changeStyle', (feature, color) => {
 			this.changeFeatureStyle(feature, color);
 		});
+		// handles draw type change, performed in the feature-sidebar
 		this.$nuxt.$on('drawType', type => {
 			this.drawType = type;
 		});
@@ -181,6 +121,79 @@ export default {
 		this.$nuxt.$off('drawType');
 	},
 	methods: {
+		initMapComponents() {
+			const raster = new TileLayer({
+				source: new OSM(),
+			});
+
+			this.source = new VectorSource({
+				features: this.loadInitFeatures(this.features)
+			});
+
+			this.vector = new VectorLayer({
+				source: this.source,
+				style: (feature, resolution) => {
+					return this.styleFunction({
+						feature,
+						pointFillColor: this.defaultColor,
+						lineColor: this.defaultColor,
+						polygonColor: this.defaultColor
+					});
+				}
+			});
+
+			this.map = new Map({
+				target: this.$refs['map-root'],
+				layers: [raster, this.vector],
+				view: new View({
+					center: this.center,
+					constrainResolution: true,
+					zoom: this.zoom,
+				}),
+			});
+
+			this.select = new Select({
+				style: null,
+				condition: click
+			});
+		},
+		fitViewToFeatures() {
+			// no need to fit view if 0 or 1 feature is present
+			if (this.source.getFeatures().length > 1) {
+				// padding, so the feature list and navbar doesn't block the view from features
+				this.map.getView().fit(this.source.getExtent(), { padding: [80, 300, 0, 0] });
+			}
+		},
+		addEventListeners() {
+			const selectedFeatures = this.select.getFeatures();
+			selectedFeatures.on('add', f => {
+				if (f !== this.getSelectedFeature) {
+					this.$store.commit('selected/change', f.element);
+				}
+			});
+			selectedFeatures.on('remove', f => {
+				this.$store.commit('selected/remove', f.element);
+			});
+
+			this.source.on('addfeature', f => {
+				f.feature.setId(new Date().getTime());
+				f.feature.set('color', this.defaultColor);
+				this.$store.commit('toggleEditState', false);
+				this.$store.commit('features/add', f.feature);
+
+				selectedFeatures.push(f.feature);
+				this.$nuxt.$emit('mapModified');
+			});
+
+			this.source.on('removefeature', f => {
+				if (f.feature === this.getSelectedFeature) {
+					this.removeBlur();
+				}
+				this.$store.commit('selected/remove', f.feature);
+				this.$store.commit('features/remove', f.feature);
+				this.$nuxt.$emit('mapModified');
+			});
+		},
 		loadInitFeatures(features) {
 			if (!features) {
 				return null;
@@ -198,7 +211,7 @@ export default {
 		} = {}) {
 			return new Style({
 				fill: polygonColor
-					? new Fill({ color: polygonColor + '33' })
+					? new Fill({ color: polygonColor + '33' }) // '33': more opaque color for fill
 					: null,
 				stroke: new Stroke({
 					color: lineColor,
@@ -213,15 +226,17 @@ export default {
 			});
 		},
 		setDrawType(type) {
-			this.map.removeInteraction(draw);
-			this.map.removeInteraction(snap);
+			this.map.removeInteraction(this.draw);
+			this.map.removeInteraction(this.snap);
 			if (type) {
-				draw = new Draw({
+				this.draw = new Draw({
 					source: this.source, type
 				});
-				this.map.addInteraction(draw);
-				snap = new Snap({ source: this.source });
-				this.map.addInteraction(snap);
+				this.map.addInteraction(this.draw);
+				this.snap = new Snap({
+					source: this.source
+				});
+				this.map.addInteraction(this.snap);
 			}
 		},
 		changeFeatureStyle(feature, color) {
@@ -235,7 +250,7 @@ export default {
 		blurFeature(feature) {
 			feature.setStyle(this.styleFunction({
 				feature,
-				pointFillColor: feature.get('color') + '80',
+				pointFillColor: feature.get('color') + '80', // opacity level
 				lineColor: feature.get('color') + '80',
 				polygonColor: feature.get('color')
 			}));
@@ -247,13 +262,6 @@ export default {
 				this.source.getFeatures().forEach(feature => {
 					this.changeFeatureStyle(feature, feature.get('color'));
 				});
-			}
-		},
-		fitViewToFeatures() {
-			// no need to fit view if 0 or 1 feature is present
-			if (this.source.getFeatures().length > 1) {
-				// padding, so the feature list and navbar doesn't block the view from features
-				this.map.getView().fit(this.source.getExtent(), { padding: [80, 300, 0, 0] });
 			}
 		}
 	}
