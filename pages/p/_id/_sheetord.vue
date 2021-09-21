81d3f4baa510c69f730e098076d9685c14c54797
Domonkos Csuzdi <csuzdi02@gmail.com>
2021-09-21 23:00:14 +0200

    minor refactor

--- pages/p/_id/_sheetord.vue
+++ pages/p/_id/_sheetord.vue
@@ -1,22 +1,12 @@
 <template>
 	<Sheet
-		:project-id="projId"
-		:sheet-ord="sheetOrd"
+		:project-id="$route.params.id"
+		:sheet-ord="$route.params.sheetord"
 	/>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			projId: null,
-			sheetOrd: null
-		};
-	},
-	created() {
-		this.projId = this.$route.params.id;
-		this.sheetOrd = this.$route.params.sheetord;
-	},
 };
 </script>
 
