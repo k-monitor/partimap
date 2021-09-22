a8c58606337885a4d49b23463fbaee29eff48f39
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-09-22 10:41:40 +0200

    fixed varchar length in migrations to be able to set up on the server

--- migrations/sqls/20210831105234-add-project-fields-up.sql
+++ migrations/sqls/20210831105234-add-project-fields-up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `project`
-	ADD COLUMN `slug` VARCHAR(255) NULL AFTER `userId`,
+	ADD COLUMN `slug` VARCHAR(191) NULL AFTER `userId`,
 	ADD COLUMN `password` VARCHAR(255) NULL AFTER `description`,
 	ADD UNIQUE INDEX `slug` (`slug`),
 	ADD INDEX `userId` (`userId`);
