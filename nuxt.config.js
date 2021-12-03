3980ad36df514c41221cd5c73c6736f9b8a11420
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-03 10:36:35 +0100

    fixing HTML lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		title: 'partimap',
+		title: 'Partimap',
 		htmlAttrs: {
-			lang: 'en'
+			lang: 'hu'
 		},
 		meta: [
 			{ charset: 'utf-8' },
