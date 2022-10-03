6026f6b9ee2af58da0235d230065a6b41e0aa5d4
FulopGergely <fgergo91@gmail.com>
2022-10-03 15:24:48 +0200

    update dropdownGroup

--- components/DropdownGroup.vue
+++ components/DropdownGroup.vue
@@ -1,30 +1,26 @@
 <template>
 	<div>
-		<div>{{this.options}}</div>
 		<select v-model="selected">
 			<option v-for="item in options" :key="item.text" :value="{value: item.text, text: item.value }">
 				{{ item.name }}
 			</option>
 		</select>
+		<div>{{this.question}}</div>
 		<b-form-select
 			v-model="selected"
-			:value="{ value: q.name, text: q.name }"
-			:name="'q' + q.id"
 			:options="options"
 			:required="q.required"
 		/>
-		<div>{{this.selected}}</div>
 		<span
-			v-if="q.required && this.selected.text == 'Egyéb'"
+			v-if="q.required && this.selected == 'other'"
 			class="text-danger"
 		>*</span>
 		<strong
-			v-if="this.selected === 'Egyéb: '"
+			v-if="this.selected == 'other'"
 			class="text-primary">Egyéb: </strong>
 		<b-form-input
-			v-if="q.type === 'dropdown' && this.selected === 'Egyéb: '"
-			v-model="selected"
-			:name="'q' + q.id"
+			v-if="this.selected == 'other'"
+			v-model="selected2"
 			:required="q.required"
 		/>
 	</div>
@@ -54,21 +50,41 @@ export default {
 		if (this.q.other) {
 			this.options.push({ text: 'Egyéb', value: 'other' });
 		}
-		console.log(this.options);
+		// console.log(this.selected);
+		// console.log(this.selected);
+		// if (this.selected.startsWith('other')) {
+		// this.options = Object.assign({}, this.q.options);
+		// this.options = Object.keys(this.options).slice(0, this.options.size).map(key => (
+		// { id: parseInt(key), text: this.options[key], value: this.options[key] }));
+		// if (this.q.other) {
+		// this.options.push({ id: this.options.length, text: 'Egyéb', value: 'other' });
+		// this.question.isSelected = false;
+		// }
 	},
 	data() {
 		return {
-			selected: this.value,
+			selected: (this.value === 'other' ? 'other' : this.value),
+			selected2: (this.value.startsWith('other') ? this.value.slice(5) : this.value),
 			a: this.answers,
 			options: this.q.options,
+			question: this.q,
 		};
 	},
 	watch: {
-		selected() {
-			// console.log(this.options[this.options.length - 1].text);
+		selected(a) {
 			console.log(this.selected);
-			this.$emit('input', this.selected);
-		}
+			if (this.value.startsWith('other')) {
+				this.$emit('input', 'other');
+			}
+			this.$emit('input', a);
+		},
+		selected2(a) {
+			console.log(this.selected2);
+			if (this.value.startsWith('other')) {
+				this.$emit('input', 'other: ' + a);
+			}
+			this.$emit('input', a);
+		},
 	},
 };
 </script>
