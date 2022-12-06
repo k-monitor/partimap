34d99be8d1d60704e271b8c6832ce424477c1933
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2022-12-06 14:27:28 +0100

    fixed interactions field type in db

---
+++ migrations/sqls/20221206132500-fix-interactions-field-down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `sheet` CHANGE COLUMN `interactions` `interactions` VARCHAR(255) NULL AFTER `features`;
---
+++ migrations/sqls/20221206132500-fix-interactions-field-up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `sheet` CHANGE COLUMN `interactions` `interactions` TEXT NULL AFTER `features`;
