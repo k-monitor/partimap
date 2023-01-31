b66c8bad3c02986b562aedc8d41351377f292bc0
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-01-31 10:07:43 +0100

    restyled toasts

--- mixins/toasts.js
+++ mixins/toasts.js
@@ -1,12 +1,16 @@
 export default {
 	methods: {
 		toast(message, variant) {
+			const colorClasses = variant === 'danger'
+				? 'bg-danger border-danger text-white'
+				: 'bg-dark border-secondary text-white';
+
 			this.$bvToast.toast(message, {
 				autoHideDelay: 2500,
+				bodyClass: `${colorClasses} border font-weight-bold rounded shadow-sm`,
 				noCloseButton: true,
 				solid: true,
 				toaster: 'b-toaster-bottom-center',
-				variant,
 			});
 		},
 		errorToast(message) {
