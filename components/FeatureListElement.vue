b3d470a663acd9f71eeeaf4d8ff37f8a6d0a186b
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-01-31 12:07:56 +0100

    added like/dislike buttons

--- components/FeatureListElement.vue
+++ components/FeatureListElement.vue
@@ -6,7 +6,7 @@
 		<b-list-group-item
 			ref="feature"
 			button
-			:class="[{ selected: selectedFeature, disabled: onEditMode, rated: !selectedFeature && !editable && rating > 0 }]"
+			:class="[{ selected: selectedFeature, disabled: onEditMode, rated: !selectedFeature && !editable && rated }]"
 			class="px-2 rounded"
 			:style="{ borderLeftColor: form.color }"
 			@click="featureClicked()"
@@ -29,7 +29,7 @@
 			>
 				<i class="fas fa-fw fa-trash" />
 			</span>
-			<span v-else-if="rating > 0">
+			<span v-else-if="rated">
 				<i class="fas fa-fw fa-check" />
 			</span>
 		</b-list-group-item>
@@ -160,7 +160,37 @@
 					/>
 				</div>
 				<b-form-group v-if="(visitor && !editable && visitorCanRate) || (!visitor && rating)">
-					<div class="border d-flex font-weight-bold justify-content-center p-1">
+					<b-button-group v-if="stars === -2" class="w-100">
+						<b-button
+							:disabled="!visitor"
+							:variant="rating === -1 ? 'danger' : 'outline-danger'"
+							@click="rating === -1 ? rating = 0 : rating = -1"
+						>
+							<i class="fas fa-thumbs-up fa-flip-both" />
+							<span
+								v-if="!visitor"
+								class="ml-2"
+							>{{ -ratingStats.dislikeCount }}</span>
+						</b-button>
+						<div class="align-items-center d-flex flex-grow-1 font-weight-bold justify-content-center">
+							{{ ratingStats.sum }}
+						</div>
+						<b-button
+							:disabled="!visitor"
+							:variant="rating === 1 ? 'success' : 'outline-success'"
+							@click="rating === 1 ? rating = 0 : rating = 1"
+						>
+							<i class="fas fa-thumbs-up" />
+							<span
+								v-if="!visitor"
+								class="ml-2"
+							>{{ ratingStats.likeCount }}</span>
+						</b-button>
+					</b-button-group>
+					<div
+						v-else
+						class="border d-flex font-weight-bold justify-content-center p-1"
+					>
 						<star-rating
 							v-model="rating"
 							:active-color="visitor ? '#ffc107' : '#17a2b8'"
@@ -267,6 +297,10 @@ export default {
 		},
 		onEditMode() {
 			return this.$store.getters.getEditState;
+		},
+		rated() {
+			const r = this.rating;
+			return Number.isInteger(r) && r !== 0;
 		},
 	},
 	watch: {
