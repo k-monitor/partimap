3dd462bf1324036ffcbd1c50c3c0348f4e180925
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-04-18 21:06:00 +0200

    using uppercase

--- locales/hu.js
+++ locales/hu.js
@@ -21,18 +21,18 @@ export default {
 	landing: {
 		title: 'TÉRKÉPES KÉRDŐÍV',
 		tagline1:
-			'A Partimap egy ingyenes, könnyen kezelhető és testreszabható térképes kérdőív azoknak,',
+			'A PARTIMAP egy ingyenes, könnyen kezelhető és testreszabható térképes kérdőív azoknak,',
 		tagline2:
 			'akik szerint a közpénzből megvalósuló fejlesztésekről meg kell kérdezni az érintetteket is.',
 		column1:
 			'Mikor kérték ki legutóbb a véleményed egy olyan fejlesztésről, amit naponta használsz? Hazai és európai uniós milliárdokból sorra újulnak meg városi tereink, közintézményeink és a közlekedési létesítmények. A társadalmi egyeztetés és az érintettek bevonása azonban sokszor csak formalitás. Pedig könnyen belátható, hogy a lebonyolítás szakmai szempontjai mellett a fejlesztéseket napi szinten használók igényeit, elvárásait is számításba kell venni ahhoz, hogy egy projekt sikeres legyen. A cél nem az, hogy modernebb legyen a vasútállomás, hanem az, hogy többen járjanak vonattal. Nem attól kap közösségi funkciót egy tér, okospadokat szerelünk fel, hanem attól, hogy a környékbeliek megkedvelik.',
 		column2:
-			'Participatory mapping, azaz részvételi térképezés: így nevezik a folyamatot, amelynek révén az érintettek pótolhatatlan tudása beépül a számukra készülő közösségi fejlesztésekbe. A Partimap térképes kérdőívező felülete ahhoz nyújt segítséget, hogy lakosokkal való kapcsolat gyorsabbá, hatékonyabbá és egyszerűbbé váljon. A program regisztráció után ingyenesen használható, saját arculattal látható el és nyílt forráskódjának köszönhetően szabadon továbbfejleszthető. Legyél állami szerv, önkormányzat, vagy helyi érdekérvényesítő csoport - ha egy okos és átlátható megoldást keresel arra, hogyan ismerd meg a környezeted véleményét, próbáld ki a Partimapot és csatlakozz te is a fejlesztéshez!',
+			'Participatory mapping, azaz részvételi térképezés: így nevezik a folyamatot, amelynek révén az érintettek pótolhatatlan tudása beépül a számukra készülő közösségi fejlesztésekbe. A PARTIMAP térképes kérdőívező felülete ahhoz nyújt segítséget, hogy lakosokkal való kapcsolat gyorsabbá, hatékonyabbá és egyszerűbbé váljon. A program regisztráció után ingyenesen használható, saját arculattal látható el és nyílt forráskódjának köszönhetően szabadon továbbfejleszthető. Legyél állami szerv, önkormányzat, vagy helyi érdekérvényesítő csoport - ha egy okos és átlátható megoldást keresel arra, hogyan ismerd meg a környezeted véleményét, próbáld ki a PARTIMAPot és csatlakozz te is a fejlesztéshez!',
 		tryButton: 'Kipróbálom',
 		tryLink: 'https://www.partimap.eu/p/Demo/0',
 		methodTitle: 'PARTIMAP részvételi módszertan',
 		methodDesc:
-			'Mitől lesz sikeres egy részvételi térképezés? Hogyan kell elkészíteni a kérdőívet, mire kell odafigyelni a terjesztés során és hogyan lehet a legtöbbet kihozni az összegyűjtött adatokból? A K-Monitor összefoglalta a legfontosabb tudnivalókat, hiszen a kérdőívezés csak egy eleme a fejlesztések társadalmasításának. A Partimap részvételi módszertana - a program használói körével együtt - a jövőben folyamatosan bővülni fog!',
+			'Mitől lesz sikeres egy részvételi térképezés? Hogyan kell elkészíteni a kérdőívet, mire kell odafigyelni a terjesztés során és hogyan lehet a legtöbbet kihozni az összegyűjtött adatokból? A K-Monitor összefoglalta a legfontosabb tudnivalókat, hiszen a kérdőívezés csak egy eleme a fejlesztések társadalmasításának. A PARTIMAP részvételi módszertana - a program használói körével együtt - a jövőben folyamatosan bővülni fog!',
 		methodButton: 'Elolvasom',
 		methodLink:
 			'https://drive.google.com/file/d/17p1JALO2iNtYNMhMkCvHCllqUTIFYrb2/view?usp=sharing',
@@ -41,7 +41,7 @@ export default {
 			{
 				title: 'Egyszerű szerkeszthetőség',
 				description:
-					'<p>A Partimappal egyszerűen lehet létrehozni többféle térképes és hagyományos kérdőíves funkciókat tartalmazó felméréseket. Munkafelületet úgy alakítottuk ki, hogy hétköznapi számítógépes felhasználói tudás elegendő legyen a kérdések megadáshoz, munkalapok létrehozásához, közösségi médiában való megosztás adatainak megadásához és egy új projekt élesítéséhez.</p><p>A projekt szerkesztőfelületébe regisztrálva az új projekt létrehozásakor először az olyan alapadatok megadására van szükség, mint a projekt címe, az adatkezelő adatai és a köszönőoldal szövege, ezt követően pedig a saját témához kapcsolódó munkalapok létrehozására van lehetőség. Ezek lehetnek szöveges oldalak (a tervek, elképzelések bemutatására), kérdőívek (feleletválasztó, legördülő menüs, szabadszavas és skálán mérhető válaszok begyűjtésére), statikus térképek (előre meghatározott térképes elemek - pontok, vonalak és poligonok - bemutatása és értékelése), valamint interaktív térképek (itt egy kérdésre válaszolva a kitöltők rajzolhatnak fel saját elemeket). A munkalapok között könnyen lehet törölni, navigálni és a sorrendjük is bármikor tetszőlegesen szerkeszthető.</p>',
+					'<p>A PARTIMAPpal egyszerűen lehet létrehozni többféle térképes és hagyományos kérdőíves funkciókat tartalmazó felméréseket. Munkafelületet úgy alakítottuk ki, hogy hétköznapi számítógépes felhasználói tudás elegendő legyen a kérdések megadáshoz, munkalapok létrehozásához, közösségi médiában való megosztás adatainak megadásához és egy új projekt élesítéséhez.</p><p>A projekt szerkesztőfelületébe regisztrálva az új projekt létrehozásakor először az olyan alapadatok megadására van szükség, mint a projekt címe, az adatkezelő adatai és a köszönőoldal szövege, ezt követően pedig a saját témához kapcsolódó munkalapok létrehozására van lehetőség. Ezek lehetnek szöveges oldalak (a tervek, elképzelések bemutatására), kérdőívek (feleletválasztó, legördülő menüs, szabadszavas és skálán mérhető válaszok begyűjtésére), statikus térképek (előre meghatározott térképes elemek - pontok, vonalak és poligonok - bemutatása és értékelése), valamint interaktív térképek (itt egy kérdésre válaszolva a kitöltők rajzolhatnak fel saját elemeket). A munkalapok között könnyen lehet törölni, navigálni és a sorrendjük is bármikor tetszőlegesen szerkeszthető.</p>',
 			},
 			{
 				title: 'Testre szabható megjelenés',
@@ -56,12 +56,12 @@ export default {
 			{
 				title: 'Fejlesztések bemutatása térképen',
 				description:
-					'<p>Adott közlekedési, infrastrukturális vagy más térképen ábrázolható fejlesztés vagy fejlesztési ötlet bemutatására és visszajelzések gyűjtésére a kérdőívhez statikus térkép munkalap adható. Ehhez más térképes felületekről (pl. Google Maps) könnyen importálhatóak korábban felrajzolt elemek, amelyekhez a Partimapon egyedi megjelenítés és értékelési lehetőség adható. Ezen felül magához a munkalaphoz is hozzáadhatóak a kérdőívnél már megismert kérdéstípusok, illetve további magyarázat, leírás, linkek. A felrajzolt térképet saját térképként elmentve egy következő projektnél is fel lehet használni.</p><p>A statikus térkép kiválóan alkalmas arra, hogy egy település különböző létesítményeihez értékelést kérjünk a lakosoktól, pl. Mennyire könnyen megközelíthető, mennyire akadálymentes, jól járható-e kerékpárral stb. Ezekből az eredmények letöltése után egy komplex értékelés rajzolódik ki. A munkalaptípus konkrét fejlesztési dilemmák eldöntésére is használható, például egy területre tervezett A és B verziójú fejlesztést megismerve dönthetnek a kitöltők, melyiket kedvelik jobban.</p>',
+					'<p>Adott közlekedési, infrastrukturális vagy más térképen ábrázolható fejlesztés vagy fejlesztési ötlet bemutatására és visszajelzések gyűjtésére a kérdőívhez statikus térkép munkalap adható. Ehhez más térképes felületekről (pl. Google Maps) könnyen importálhatóak korábban felrajzolt elemek, amelyekhez a PARTIMAPon egyedi megjelenítés és értékelési lehetőség adható. Ezen felül magához a munkalaphoz is hozzáadhatóak a kérdőívnél már megismert kérdéstípusok, illetve további magyarázat, leírás, linkek. A felrajzolt térképet saját térképként elmentve egy következő projektnél is fel lehet használni.</p><p>A statikus térkép kiválóan alkalmas arra, hogy egy település különböző létesítményeihez értékelést kérjünk a lakosoktól, pl. Mennyire könnyen megközelíthető, mennyire akadálymentes, jól járható-e kerékpárral stb. Ezekből az eredmények letöltése után egy komplex értékelés rajzolódik ki. A munkalaptípus konkrét fejlesztési dilemmák eldöntésére is használható, például egy területre tervezett A és B verziójú fejlesztést megismerve dönthetnek a kitöltők, melyiket kedvelik jobban.</p>',
 			},
 			{
 				title: 'Most te jössz - rajzolj a térképre!',
 				description:
-					'<p>A Partimap egyediségét az adja más térképes felületekhez képest, hogy a kitöltők térképre rajzolva is kifejezhetik a véleményüket, illusztrálhatják a javaslataikat, vagy egyszerűen megoszthatják, merre közlekednek. Egy település, kerület vagy környék köztereit számos ember használja, akiknek szerteágazó térhasználati igényei és véleményei felmérésére alkalmas az interaktív térképes munkalap. A megkérdezett felhasználók pont, vonal és alakzat segítségével adhatnak grafikus választ a feltett kérdésekre. A bejövő eredményeket könnyen fel lehet dolgozni akár egy hőtérkép formájában, vagy akár egy következő kérdőívben a beérkezett javaslatokat lehet egy statikus térképen szavazásra bocsátani.</p>',
+					'<p>A PARTIMAP egyediségét az adja más térképes felületekhez képest, hogy a kitöltők térképre rajzolva is kifejezhetik a véleményüket, illusztrálhatják a javaslataikat, vagy egyszerűen megoszthatják, merre közlekednek. Egy település, kerület vagy környék köztereit számos ember használja, akiknek szerteágazó térhasználati igényei és véleményei felmérésére alkalmas az interaktív térképes munkalap. A megkérdezett felhasználók pont, vonal és alakzat segítségével adhatnak grafikus választ a feltett kérdésekre. A bejövő eredményeket könnyen fel lehet dolgozni akár egy hőtérkép formájában, vagy akár egy következő kérdőívben a beérkezett javaslatokat lehet egy statikus térképen szavazásra bocsátani.</p>',
 			},
 			{
 				title: 'Átjárhatóság más felületekkel',
@@ -76,12 +76,12 @@ export default {
 			{
 				title: 'Ingyenes, szabadon felhasználható',
 				description:
-					'<p>Az alkalmazás használata ingyenes, gyors regisztrációt követően máris létrehozható egy saját Partimap kérdőív. A K-Monitor és partnerei által fejlesztett eszköz nyílt forráskóddal rendelkezik, így szabadon továbbfejleszthető, alakítható és felhasználható. Célunk, hogy minél több olyan projekt szülessen, ami ösztönzi az állampolgárok bevonását a közügyek eldöntésébe, így kifejezetten örülünk, ha az eszköz kreatív alkalmazásának, fejlesztésének. </p>',
+					'<p>Az alkalmazás használata ingyenes, gyors regisztrációt követően máris létrehozható egy saját PARTIMAP kérdőív. A K-Monitor és partnerei által fejlesztett eszköz nyílt forráskóddal rendelkezik, így szabadon továbbfejleszthető, alakítható és felhasználható. Célunk, hogy minél több olyan projekt szülessen, ami ösztönzi az állampolgárok bevonását a közügyek eldöntésébe, így kifejezetten örülünk, ha az eszköz kreatív alkalmazásának, fejlesztésének. </p>',
 			},
 			{
 				title: 'Szeretném segíteni a projektet!',
 				description:
-					'<p>A Partimap szoftvert a K-Monitor egyesület az Európai Bizottság (DG Regio) támogatásából valósította meg 2021-ben. A jelenlegi verzió publikálásával a projekt azonban korántsem ért véget! Az ingyenes, szabadon felhasználható felületet állami szerveknek, önkormányzatoknak, érdekérvényesítő civil szervezeteknek készítettük azzal a céllal, hogy az eszköz segíti őket a fejlesztésekkel kapcsolatos döntések társadalmasításában, így közös vagyonunkkal takarékosabban gazdálkodhatunk.</p><p>Ha érdekel a projekt, és szeretnél bekapcsolódni a fejlesztésbe, vagy csak megosztanád velünk a gondolataidat a PARTIMAP-ról, írj nekünk a <a href="mailto:hello@partimap.eu-ra.">hello@partimap.eu-ra.</a>-ra. Ha anyagilag támogatnád a fejlesztést, hogy az ingyenes, szabadon felhasználható szoftver minél többek számára legyen elérhető, <a href="https://tamogatas.k-monitor.hu/" target="_blank">támogasd a K-Monitort!</a></p>',
+					'<p>A PARTIMAP szoftvert a K-Monitor egyesület az Európai Bizottság (DG Regio) támogatásából valósította meg 2021-ben. A jelenlegi verzió publikálásával a projekt azonban korántsem ért véget! Az ingyenes, szabadon felhasználható felületet állami szerveknek, önkormányzatoknak, érdekérvényesítő civil szervezeteknek készítettük azzal a céllal, hogy az eszköz segíti őket a fejlesztésekkel kapcsolatos döntések társadalmasításában, így közös vagyonunkkal takarékosabban gazdálkodhatunk.</p><p>Ha érdekel a projekt, és szeretnél bekapcsolódni a fejlesztésbe, vagy csak megosztanád velünk a gondolataidat a PARTIMAP-ról, írj nekünk a <a href="mailto:hello@partimap.eu-ra.">hello@partimap.eu-ra.</a>-ra. Ha anyagilag támogatnád a fejlesztést, hogy az ingyenes, szabadon felhasználható szoftver minél többek számára legyen elérhető, <a href="https://tamogatas.k-monitor.hu/" target="_blank">támogasd a K-Monitort!</a></p>',
 			},
 		],
 		funding:
@@ -242,7 +242,7 @@ export default {
 		colorAdd: 'Szín beállítása',
 		colorDel: 'Szín törlése (alapértelmezés)',
 		colorDescription:
-			'A kérdőív vezérlőgombjainak és linkjeinek színét lehet itt beállítani. Az alapértelmezés a Partimap kékje.',
+			'A kérdőív vezérlőgombjainak és linkjeinek színét lehet itt beállítani. Az alapértelmezés a PARTIMAP kékje.',
 		confirmDeleteUser: 'Fiók törlése',
 		deleteConfirmation:
 			'<strong>Jól meggondoltam,</strong> törlöm a(z) <strong>{email}</strong> fiókot és annak minden adatát (térképek, projektek, beérkezett adatok, képek).',
