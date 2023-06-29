67bceec2f69c1557b7658d43a0e4a59f6a18efcc
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-06-29 14:48:02 +0200

    implemented submitted feature viewer page

---
+++ pages/admin/project/_id/submitted-features/_sheetord.vue
@@ -0,0 +1,70 @@
+<template>
+	<div>
+		<client-only>
+			<Map
+				:key="$route.path"
+				:features="loadInitFeatures()"
+			/>
+		</client-only>
+		<Sidebar
+			admin
+			:back-label="$t('sheetEditor.back')"
+			:loading="loading"
+			@back="back"
+		>
+			<FeatureList />
+		</Sidebar>
+	</div>
+</template>
+
+<script>
+import GeoJSON from 'ol/format/GeoJSON';
+
+export default {
+	middleware: ['auth'],
+	async asyncData({ $axios, store, params, redirect, localePath }) {
+		store.commit('features/clear');
+		try {
+			const project = await $axios.$get(`/api/project/${params.id}`);
+			const sheet = project.sheets[params.sheetord]; // sheets are ordered on server
+			const submissions = await $axios.$get(
+				'/api/submitted-features/' + sheet.id
+			);
+			const features = submissions
+				.map(s => JSON.parse(s.features))
+				.flat();
+			return { features, project, sheet };
+		} catch (error) {
+			redirect(localePath('/admin/projects'));
+		}
+	},
+	data() {
+		return {
+			loading: true,
+		};
+	},
+	head() {
+		return {
+			title: `Admin: ${this.sheet?.title}`,
+		};
+	},
+	mounted() {
+		this.$store.commit('selected/clear');
+		this.loading = false;
+	},
+	methods: {
+		back() {
+			this.$router.push(
+				this.localePath(`/admin/project/${this.project.id}`)
+			);
+		},
+		loadInitFeatures() {
+			const features = this.features;
+			const featureCollection = { type: 'FeatureCollection', features };
+			return features
+				? new GeoJSON().readFeatures(featureCollection)
+				: null;
+		},
+	},
+};
+</script>
