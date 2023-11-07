396b4b5dbde2bd18fca5faa758c3bbd76f916b9a
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-11-07 09:23:10 +0100

    wired in lt translation on server

--- server/common/i18n.js
+++ server/common/i18n.js
@@ -1,9 +1,11 @@
 const en = require('../../locales/en-server');
 const es = require('../../locales/es-server');
 const hu = require('../../locales/hu-server');
+const lt = require('../../locales/lt-server');
 
 module.exports = locale => {
 	if (locale === 'es') return es;
 	if (locale === 'hu') return hu;
+	if (locale === 'lt') return lt;
 	return en;
 };
