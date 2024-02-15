f731bde4d908e7fbb8bab516b426ca4a8e9a874c
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-02-15 17:20:21 +0100

    moved out star rating

--- components/FeatureList/FeatureRatingControls.vue
+++ components/FeatureList/FeatureRatingControls.vue
@@ -1,51 +1,14 @@
 <template>
-	<div class="mb-3">
-		<template v-if="interactions.stars === -2">
-			<LikeDislikeRating
-				v-model="rating"
-				:show-results="showResults"
-			/>
-		</template>
-
-		<input
-			v-model.number="rating"
-			type="number"
-		/>
-		<!--
-				<b-button-group
-					v-if="stars === -2"
-					class="w-100"
-				>
-
-				</b-button-group>
-				<div
-					v-else
-					class="border d-flex font-weight-bold justify-content-center p-1"
-				>
-					<StarRating
-						v-model="rating"
-						active-color="var(--brand)"
-						:animate="visitor && !showResults"
-						border-color="var(--brand)"
-						:border-width="2"
-						clearable
-						inactive-color="#fff"
-						:max-rating="stars"
-						:read-only="!visitor || showResults"
-						:round-start-rating="false"
-						:show-rating="false"
-						:star-size="16"
-					/>
-					<span
-						v-if="!visitor || showResults"
-						class="ml-2"
-					>
-						{{ rating ? Number(rating).toFixed(1) : '-' }}
-						(<small class="fas fa-user fa-fw" />
-						{{ initFeatureRating.count || 0 }})
-					</span>
-				</div> -->
-	</div>
+	<LikeDislikeRating
+		v-if="interactions.stars === -2"
+		v-model="rating"
+		:show-results="showResults"
+	/>
+	<StarRating
+		v-else
+		v-model="rating"
+		:show-results="showResults"
+	/>
 </template>
 
 <script>
