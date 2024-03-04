5b34c219d755b7543cbe6c0d38f97581a57e3b6e
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-03-04 16:21:05 +0100

    added missing languages

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,11 +1,15 @@
 import en from '~/locales/en.js';
+import es from '~/locales/es.js';
 import hu from '~/locales/hu.js';
+import lt from '~/locales/lt.js';
 
 export default defineI18nConfig(() => ({
 	fallbackLocale: 'en',
 	legacy: false,
 	messages: {
 		en,
+		es,
 		hu,
+		lt,
 	},
 }));
