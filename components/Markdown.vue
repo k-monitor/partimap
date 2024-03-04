37fdb1474f86c8d6b635a8b5c4e7b118331fc4b3
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-03-04 16:21:19 +0100

    added help page stub

---
+++ components/Markdown.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import markdownIt from 'markdown-it';
+import markdownItAttrs from 'markdown-it-attrs';
+
+const markdownItInstance = markdownIt({ html: true });
+markdownItInstance.use(markdownItAttrs);
+
+const props = defineProps<{
+	md: string;
+}>();
+
+const html = computed(() => markdownItInstance.render(props.md));
+</script>
+
+<template>
+	<!-- eslint-disable vue/no-v-html -->
+	<div
+		class="md"
+		v-html="html"
+	/>
+</template>
---
+++ pages/hogyan-mukodik.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineI18nRoute({
+	paths: {
+		en: '/how-it-works',
+		es: '/guia-del-usuario',
+		lt: '/naudojimosi-instrukcija',
+	},
+});
+
+const { t } = useI18n();
+
+useHead({
+	title: t('help.title'),
+});
+
+const editorsHelp = ref(''); // TODO GET /api/i18n/get/${lang}/editorsHelp
+const visitorsHelp = ref(''); // TODO GET /api/i18n/get/${lang}/visitorsHelp
+</script>
+
+<template>
+	<PublicFrame>
+		<div class="container my-5 help">
+			<header class="my-5">
+				<h1 class="border-bottom">{{ $t('help.title') }}</h1>
+			</header>
+
+			<Markdown
+				class="help mb-5"
+				:md="visitorsHelp"
+			/>
+
+			<Markdown
+				class="help mb-5"
+				:md="editorsHelp"
+			/>
+		</div>
+	</PublicFrame>
+</template>
