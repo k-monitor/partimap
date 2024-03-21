1fe090691b05d5cb04ae6feb0fe83101af28e011
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-03-21 09:49:10 +0100

    added register page

---
+++ components/Terms.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps<{
+	projectDataProcessor: string;
+}>();
+
+const { t } = useI18n();
+
+const md = computed(() => {
+	return t('Terms.content').replace(
+		'$_PROJECT_DATA_PROCESSOR_$',
+		props.projectDataProcessor || '',
+	);
+});
+</script>
+
+<template>
+	<Markdown
+		class="terms"
+		:md="md"
+	/>
+</template>
+
+<style>
+.terms h1,
+.terms h2 {
+	font-size: 1.1rem;
+	font-weight: bold;
+}
+
+.terms h2 {
+	margin-top: 2rem;
+}
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,141 @@
+<script setup lang="ts">
+definePageMeta({
+	middleware: ['public-only'],
+});
+
+const localePath = useLocalePath();
+const { locale, t } = useI18n();
+
+useHead({
+	title: t('register.title'),
+});
+
+const email = ref('');
+const emailInput = ref<HTMLInputElement>();
+const name = ref('');
+const password = ref('');
+const consent = ref(false);
+const loading = ref(true);
+const termsModal = ref(false);
+
+onMounted(async () => {
+	loading.value = false;
+	emailInput.value?.focus();
+});
+
+const { executeReCaptcha } = useReCaptcha();
+const { errorToast } = useToasts();
+
+async function userReg() {
+	try {
+		loading.value = true;
+		const captcha = await executeReCaptcha('register');
+		await $fetch('/api/user/register', {
+			method: 'POST',
+			body: {
+				captcha,
+				consent: consent.value,
+				email: email.value,
+				locale: locale.value,
+				name: name.value,
+				password: password.value,
+			},
+		});
+		navigateTo(
+			localePath({
+				path: 'login',
+				query: { registered: null },
+			}),
+		);
+	} catch (err) {
+		errorToast(t('register.registrationFailed'));
+	} finally {
+		loading.value = false;
+	}
+}
+</script>
+
+<template>
+	<!-- eslint-disable vue/no-v-html -->
+	<div class="container d-flex flex-column flex-grow-1">
+		<div class="row flex-grow-1">
+			<div class="col col-sm-10 col-md-8 col-lg-6 m-auto">
+				<form @submit.prevent="userReg">
+					<div class="card shadow-sm">
+						<CardHeader :text="$t('register.title')" />
+						<div class="card-body">
+							<form-group :label="$t('register.email')">
+								<input
+									ref="emailInput"
+									v-model="email"
+									class="form-control"
+									required
+									type="email"
+								/>
+							</form-group>
+							<form-group :label="$t('register.name')">
+								<input
+									v-model="name"
+									class="form-control"
+									required
+								/>
+							</form-group>
+							<form-group :label="$t('register.password')">
+								<input
+									v-model="password"
+									class="form-control"
+									required
+									type="password"
+								/>
+							</form-group>
+							<form-group>
+								<b-form-checkbox
+									v-model="consent"
+									name="consent"
+									required
+								>
+									{{ $t('register.consent1') }}
+									<a
+										href="javascript:void(0)"
+										@click.stop="termsModal = true"
+										v-html="$t('register.consent2')"
+									/>
+								</b-form-checkbox>
+							</form-group>
+
+							<p class="m-0 small text-muted">
+								{{ $t('register.procedure') }}
+							</p>
+						</div>
+						<div class="card-footer d-flex justify-content-between">
+							<b-button
+								:to="localePath('/login')"
+								variant="link"
+							>
+								{{ $t('register.login') }}
+							</b-button>
+							<b-button
+								type="submit"
+								variant="primary"
+							>
+								{{ $t('register.submit') }}
+							</b-button>
+						</div>
+						<LoadingOverlay :show="loading" />
+					</div>
+				</form>
+			</div>
+			<client-only>
+				<b-modal
+					v-model="termsModal"
+					hide-footer
+					scrollable
+					size="lg"
+					:title="$t('register.termsTitle')"
+				>
+					<Terms />
+				</b-modal>
+			</client-only>
+		</div>
+	</div>
+</template>
