2d18d30807f37409d84de528f563085bedc40c40
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-05-24 22:39:40 +0200

    rebuilt file input to provide custom placeholder

--- components/ImageFileInput.vue
+++ components/ImageFileInput.vue
@@ -1,18 +1,64 @@
 <script setup lang="ts">
-const image = defineModel<File | File[] | null | undefined>();
+const image = defineModel<File | null | undefined>();
 
-defineProps<{
-	imageState?: boolean | null | undefined;
-}>();
+const props = withDefaults(
+	defineProps<{
+		state: boolean | null;
+	}>(),
+	{
+		state: null,
+	},
+);
+
+const input = ref<HTMLInputElement>();
+
+function handleChange(e: Event) {
+	console.log('change called');
+	const fileInputEl = e.target as HTMLInputElement;
+	if (fileInputEl.files?.[0]) {
+		image.value = fileInputEl.files[0];
+	}
+}
+
+function handleDrop(e: DragEvent) {
+	if (e.dataTransfer?.files && input.value) {
+		console.log('e', e.dataTransfer.files);
+		image.value = e.dataTransfer.files[0];
+		console.log('i', image.value);
+		nextTick(() => console.log('i2', image.value));
+	}
+	dropzoneActive.value = false;
+}
+
+const dropzoneActive = ref(false);
+
+const { t } = useI18n();
+
+const label = computed(() => {
+	if (dropzoneActive.value) return t('imageUpload.dropzone');
+	if (image.value) return image.value.name;
+	return t('imageUpload.browse');
+});
 </script>
 
 <template>
-	<b-form-file
-		v-model="image"
-		accept="image/jpeg, image/png, image/webp"
-		:drop-placeholder="$t('imageUpload.dropzone')"
-		:placeholder="$t('imageUpload.browse')"
-		:state="imageState"
-	/>
-	<!-- FIXME ^ placeholder prop is not used -->
+	<label
+		class="form-control"
+		:class="{
+			'is-invalid': state === false,
+			'is-valid': state,
+		}"
+		@dragover.prevent="dropzoneActive = true"
+		@dragleave.prevent="dropzoneActive = false"
+		@drop.prevent="handleDrop"
+	>
+		<span>{{ label }}</span>
+		<input
+			ref="input"
+			accept="image/jpeg, image/png, image/webp"
+			style="height: 0; width: 0; opacity: 0"
+			type="file"
+			@change="handleChange"
+		/>
+	</label>
 </template>
