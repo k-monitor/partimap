9f62f174cb33163da584701d8ec23eca042cce46
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-11-21 10:43:22 +0100

    rebuilt banner from separate layers

--- components/TheBanner.vue
+++ components/TheBanner.vue
@@ -1,26 +1,64 @@
 <template>
 	<header
-		class="d-flex flex-column text-white"
-		style="background-color: #007bff"
+		class="py-4"
+		style="
+			background-color: #007bff;
+			background-image: url('/banner-grid.png');
+			background-position: center;
+			background-size: auto 320px;
+		"
 	>
-		<div id="header-img"></div>
-		<div class="container pb-3 text-center">
-			<p class="fw-bold lead mb-0">
-				{{ $t('landing.tagline1') }}
-				<br class="d-none d-lg-inline" />
-				{{ $t('landing.tagline2') }}
-			</p>
+		<div style="display: grid; grid-template-columns: 1fr auto 1fr">
+			<div style="position: relative">
+				<img
+					src="/banner-left.png"
+					alt=""
+					style="
+						bottom: -36px;
+						height: 250px;
+						object-fit: cover;
+						object-position: right bottom;
+						position: absolute;
+						width: 100%;
+					"
+				/>
+			</div>
+			<div class="container flex-grow-1 py-3 text-center text-white">
+				<img
+					src="/logo-white.png"
+					alt=""
+					style="height: 160px"
+				/>
+				<p
+					class="fw-bold lead mx-auto my-0 p-3"
+					style="max-width: 55ch"
+				>
+					{{ $t('landing.tagline1') }}
+					{{ $t('landing.tagline2') }}
+				</p>
+			</div>
+			<div style="position: relative">
+				<img
+					src="/banner-right.png"
+					alt=""
+					style="
+						bottom: 16px;
+						height: 250px;
+						object-fit: cover;
+						object-position: left bottom;
+						position: absolute;
+						width: 100%;
+					"
+				/>
+			</div>
 		</div>
+		<div
+			style="
+				background-image: url('/banner-road.png');
+				background-repeat: repeat-x;
+				background-size: contain;
+				height: 36px;
+			"
+		></div>
 	</header>
 </template>
-
-<style scoped>
-#header-img {
-	background-image: url('/banner.png');
-	background-position: center;
-	background-repeat: repeat-x;
-	--size: 320px;
-	background-size: auto var(--size);
-	height: var(--size);
-}
-</style>
