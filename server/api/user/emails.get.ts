28a7d35a812cdaa8d5ce2bb3f25bff89cf30eb32
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2025-07-21 09:33:48 +0200

    added email list endpoint for admins

---
+++ server/api/user/emails.get.ts
@@ -0,0 +1,23 @@
+import * as db from '~/server/data/users';
+
+export default defineEventHandler(async (event) => {
+	await ensureLoggedIn(event);
+	await ensureAdmin(event);
+
+	const users = await db.findAll();
+	const emails = users
+		.filter((user) => user.active)
+		.map((user) => user.email)
+		.join('\n');
+
+	const filename = `partimap-emails-${new Date()
+		.toISOString()
+		.slice(2, 16)
+		.replace(/[-T:]/g, '')
+		.replace(/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})$/, '$1$2$3-$4$5')}.txt`;
+
+	setResponseHeader(event, 'Content-Type', 'text/plain; charset=utf-8');
+	setResponseHeader(event, 'Content-Disposition', `attachment; filename="${filename}"`);
+
+	return emails;
+});
