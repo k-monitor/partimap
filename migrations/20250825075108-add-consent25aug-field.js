d84ba54d4c0b78750c543429d2955fc978c5c61a
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2025-08-25 13:37:46 +0200

    fixed migration filename

--- migrations/20250825075108-add-consent25aug-field.js
+++ migrations/20250825075108-add-consent25aug-field.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20250825075108-add-consent25Aug-field-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20250825075108-add-consent25aug-field-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
