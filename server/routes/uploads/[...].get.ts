5f15c2b01e6ebd3eebd2c55a3beb9c8025bfeeb0
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2025-12-12 11:17:02 +0100

    fixed extra security

--- server/routes/uploads/[...].get.ts
+++ server/routes/uploads/[...].get.ts
@@ -6,6 +6,9 @@ export default defineEventHandler(async (event) => {
 	const baseDir = path.resolve(process.cwd());
 	const file = path.resolve(baseDir, 'uploads', event.context.params?._ || '');
 	if (!file.startsWith(baseDir + path.sep)) {
+		throw createError({ status: 403, message: 'Forbidden' });
+	}
+	if (!fs.existsSync(file) || !fs.statSync(file).isFile()) {
 		throw createError({ status: StatusCodes.NOT_FOUND });
 	}
 	return sendStream(event, fs.createReadStream(file));
