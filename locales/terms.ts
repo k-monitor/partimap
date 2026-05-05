ea7a8c50b8e77d483e8b95fec3e423817f333435
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2026-05-05 15:54:52 +0200

    added missing es-terms to logic

--- locales/terms.ts
+++ locales/terms.ts
@@ -1,9 +1,11 @@
-import en from './en-terms.md?raw';
 import de from './de-terms.md?raw';
+import en from './en-terms.md?raw';
+import es from './es-terms.md?raw';
 import hu from './hu-terms.md?raw';
 
 export default function getTermsContent(locale: string) {
 	if (locale === 'de') return de;
+	if (locale === 'es') return es;
 	if (locale === 'hu') return hu;
 	return en;
 }
