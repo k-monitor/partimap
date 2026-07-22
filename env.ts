d18b8bd88cb8115942370c70ea85432da3b9687f
Zsolt Jurányi <juzraai@users.noreply.github.com>
2026-07-22 16:43:08 +0200

    allow passwordless local smtp servers

--- env.ts
+++ env.ts
@@ -29,10 +29,10 @@ export const env = createEnv({
 		// Mail
 		SMTP_FROM: z.string().min(1),
 		SMTP_REPLY_TO: z.string().min(1),
-		SMTP_HOST: z.string().min(1),
+		SMTP_HOST: z.string().min(1).default('localhost'),
 		SMTP_PORT: z.coerce.number(),
-		SMTP_USER: z.string().min(1).optional(),
-		SMTP_PASS: z.string().min(1).optional(),
+		SMTP_USER: z.string().optional(),
+		SMTP_PASS: z.string().optional(),
 
 		// Captcha
 		NUXT_TURNSTILE_SECRET_KEY: z.string().min(1).optional(),
